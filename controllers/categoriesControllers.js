ad02ded96546afcb33b6bfe5e16ae6b89fd5dd00
Andrew <asaljakin@gmail.com>
2024-06-06 22:25:41 +0300

    update/limits&total in Categories

--- controllers/categoriesControllers.js
+++ controllers/categoriesControllers.js
@@ -2,8 +2,18 @@ import * as categoriesService from "../services/categoriesServices.js";
 import controllerWrapper from "../decorators/controllerWrapper.js";
 
 const getCategories = async (req, res) => {
-  const categories = await categoriesService.findCategories();
-  res.json(categories);
+  const filter = {};
+  const fields = "";
+  const { page = 1, limit = 11 } = req.query;
+  const skip = (page - 1) * limit;
+  const settings = { skip, limit };
+  const categories = await categoriesService.findCategories({
+    filter,
+    fields,
+    settings,
+  });
+  const total = await categoriesService.countCategories(filter);
+  res.json({ total, categories });
 };
 
 export default {
