91badcc1d9fac2eb91a0fa811e39a511d07676e7
Dmytro <tschernomord@gmail.com>
2024-06-12 20:39:53 +0200

    populate owner testimonials

--- services/testimonialsServices.js
+++ services/testimonialsServices.js
@@ -1,3 +1,4 @@
 import Testimonial from "../models/Testimonials.js";
 
-export const findTestimonials = () => Testimonial.find();
+export const findTestimonials = () =>
+    Testimonial.find().populate("owner", "name");
